16ec076742a7052004ee3b690c19cc6fd0cff0d4
Chris Dedeian <chrisdedeian@gmail.com>
2018-10-14 17:42:07 -0700

    Added competitive play stats

--- main.py
+++ main.py
@@ -29,24 +29,30 @@ def lambda_handler(event, context):
 
     
 def web_scrape(platform, username):
-    res = {}
+    res = {
+        'quickplay' : {},
+        'competitive' : {}
+    }
     page = requests.get('https://playoverwatch.com/en-us/career/' + platform + '/' + username)
     tree = html.fromstring(page.content)
     
     no_profile = tree.xpath("//h1[@class='u-align-center']/text()")
     private_profile = tree.xpath("//h3[@class='h4']/text()")
     if(no_profile):
-        res['error'] = "Profile does not exist (Make sure you include the ID of your username. e.x.: " + username + "-12345)"
+        res['error'] = "Profile does not exist. (Make sure you include the ID of your username. e.x.: " + username + "-12345)"
     elif(private_profile):
         res['error'] = "Profile is private. Overwatch defaults profiles to private. To make it public, in game go to Settings > Social > Set career profile to public. Exit game for changes to be made"
     else:
         titles = tree.xpath("//div[@data-group-id='stats'][1]//div//div[@class='card-stat-block']//table[@class='data-table']//tbody//tr//td[1]/text()")
         values = tree.xpath("//div[@data-group-id='stats'][1]//div//div[@class='card-stat-block']//table[@class='data-table'][1]//tbody//tr//td[2]/text()")
-        print("Title", titles)
-        print("Values", values)
+        cp_titles = tree.xpath("//div[@id='competitive']//section//div[@data-group-id='stats'][1]/div//div[@class='card-stat-block']//table[@class='data-table']//tbody//tr//td[1]/text()")
+        cp_values = tree.xpath("//div[@id='competitive']//section//div[@data-group-id='stats'][1]/div//div[@class='card-stat-block']//table[@class='data-table']//tbody//tr//td[2]/text()")
 
         for i in range(0, len(titles)):
-            res[titles[i]] = values[i]
-    
+            res['quickplay'][titles[i]] = values[i]
+
+        for i in range(0, len(cp_titles)):
+            res['competitive'][cp_titles[i]] = values[i]
+
     return res
 
